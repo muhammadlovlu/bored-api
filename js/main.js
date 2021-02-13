ecf95fa04de7d95a69ab392c9815508d0657f729
muhammadlovlu <chyshahab@gmail.com>
2021-02-13 16:51:38 +0600

    updated

--- js/main.js
+++ js/main.js
@@ -1,8 +1,11 @@
-fetch("http://www.boredapi.com/api/activity/")
-.then(response => response.json())
-.then(data =>{
-   document.getElementById('headerAutomate').innerText = data.activity;
-   setInterval(() => {
-       
-   }, 10000);
-})
+function executeAutomate() {
+    fetch("http://www.boredapi.com/api/activity/")
+        .then(response => response.json())
+        .then(data => {
+            document.getElementById('headerAutomate').innerText = data.activity;
+        })
+}
+executeAutomate();
+setInterval(() => {
+    executeAutomate();
+}, 10000);
