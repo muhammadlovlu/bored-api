4d7fc22e7e3b8a19873c80f56b5fab385c680f58
muhammadlovlu <chyshahab@gmail.com>
2021-02-13 23:20:23 +0600

    updated

--- js/random.js
+++ js/random.js
@@ -1,24 +1,21 @@
-// function generatePassword() {
-//     fetch("https://randomuser.me/api/?password=special,32")
-//         .then(res => res.json())
-//         .then(data => {
-//             const password = document.getElementById('password');
-//             password.innerText = data.info.seed;
-//         })
-// }
-// generatePassword();
-// setInterval(() => {
-//     generatePassword();
-// }, 1000);
+// fetch("https://randomuser.me/api")
+// .then(res => res.json())
+// .then(data => {
+//     console.log(data)
+// })
 
-
-
-
-fetch("https://randomuser.me/api")
-.then (res => res.json())
-.then(data => {
-    console.log(data)
-})
+function generatePassword() {
+    fetch("https://randomuser.me/api/?password=special,32")
+        .then(res => res.json())
+        .then(data => {
+            const password = document.getElementById('password');
+            password.innerText = data.info.seed;
+        })
+}
+generatePassword();
+setInterval(() => {
+    generatePassword();
+}, 1000);
 
 function userNameCall() {
     fetch("https://randomuser.me/api")
@@ -26,11 +23,17 @@ function userNameCall() {
         .then(data => {
             const firstName = document.getElementById("first-name");
             firstName.innerText = data.results[0].name.first;
-            const lastName =document.getElementById("last-name");
+            const lastName = document.getElementById("last-name");
             lastName.innerText = data.results[0].name.last;
             const city = document.getElementById("city");
             city.innerText = data.results[0].location.city;
+            const country = document.getElementById("country");
+            country.innerText = data.results[0].location.country;
+            console.log(country.innerText);
 
         })
-    }
+}
+userNameCall();
+setInterval(() => {
     userNameCall();
+}, 1000);
